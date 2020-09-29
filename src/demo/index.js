208a19167b079f27d422a5500fefb9581ef89cbf
ShiraRotman <shiranetmail@gmail.com>
2020-09-29 08:05:06 +0300

    Initial commit - basic demo page.

---
+++ src/demo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function PinnuleDemo() 
+{ return "Demos will be added here as components are developed..."; }
+
+ReactDOM.render(<React.StrictMode><PinnuleDemo/></React.StrictMode>,document.
+		getElementById("componentsDemoRoot"));
